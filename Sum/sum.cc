00d7c8f4c81b3319fe80fe67e7df18ca599df7df
Stefano Geercken <stefano.geercken@gmail.com>
2023-01-26 13:07:37 +0100

    sum second version

--- Sum/sum.cc
+++ Sum/sum.cc
@@ -1,14 +1,21 @@
 #include <iostream>
 
+int suma(const int num)
+{
+	int sum=0;
+	for(int i=0; i<=num;i++){
+		sum+=i;
+	}	
+	return sum;
+	
+}
+
 int main()
 {
 	std::cout << "Enter a number: " << std::endl;
 	int num;
-	int sum =0;
 	std::cin >> num;
-	for(int i=0; i<=num;i++){
-		sum+=i;
-	}
+	int sum = suma(num);
 	std::cout << sum << std::endl;
 	return 0;
 }
