95dfa0cc7a438dea02ff8fb33a82da577a1fd391
Stefano Geercken <stefano.geercken@gmail.com>
2023-01-26 12:33:58 +0100

    second version

--- HelloWorld/hello.cc
+++ HelloWorld/hello.cc
@@ -1,7 +1,11 @@
 #include <iostream>
+#include<string>
 
 int main()
 {
-	std::cout << "Hello World" << std::endl;
+	std::cout << "Whats your name?" << std::endl;
+	std::string nombre;
+	std::cin >> nombre;
+	std::cout << "Hello " << nombre << "!" << std::endl;
 	return 0;
 }	
