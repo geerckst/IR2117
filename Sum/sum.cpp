123c0dd1f55af75adc21166d0101d5723c2dff77
geerckst <stefano.geercken@gmail.com>
2023-01-30 19:23:31 +0100

    sum (version5)

--- Sum/sum.cpp
+++ Sum/sum.cpp
@@ -13,14 +13,13 @@ int main(){
 	int sum=0;
 	int num;
 	std::cin >> num;
-	if(num>=1){
-		int sum = fsum(num);
-		std::cout << "The sum from 1 to " << num << " is " << sum << std::endl;
-		return 0;
-	} else {
-		std::cout << "Error, entered number is smaller than 1!" << std::endl;
-		return 0;
-	}
-
-	}
+	while(num<1){
+		std::cout << "Error, entered number is smaller than 1, please try again!" << std::endl;
+		std::cout << "Enter a number: ";
+		std::cin >> num;
+		}
+	sum = fsum(num);
+	std::cout << "The sum from 1 to " << num << " is " << sum << std::endl;
+	return 0;
+}
 	
