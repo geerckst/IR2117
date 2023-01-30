9ced4e9de7a797db534c676786fcf971d6346a4d
geerckst <stefano.geercken@gmail.com>
2023-01-30 18:10:55 +0100

    sum (version1)

---
+++ Sum/sum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Enter a number: ";
+	int num;
+	int sum = 0;
+	std::cin >> num;
+	for(int i=0; i<=num;i++){
+		sum += i;
+	}
+	std::cout << "The sum from 1 to " << num << " is " << sum << std::endl;
+	return 0;
+}
