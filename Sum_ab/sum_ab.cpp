815d52e6c94d6c033e3f614dcbea4a483857800d
geerckst <stefano.geercken@gmail.com>
2023-01-31 11:52:48 +0100

    sum_ab (version3)

--- Sum_ab/sum_ab.cpp
+++ Sum_ab/sum_ab.cpp
@@ -16,6 +16,12 @@ int main(){
 	std::cout << "Enter b number >=1: ";
 	std::cin >> b;
 	
+	while(b<1){
+		std::cout << "Error, b is smaller than 1, try again!" << "\n";
+		std::cout << "Enter b number >=1: ";
+		std::cin >> b;
+	}
+	
 	int sum = 0;
 	
 	for(int i=a; i<=b; i++){
