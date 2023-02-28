25df23c1a6d8187cbdc72a24d81f54f3959d374b
geerckst <stefano.geercken@gmail.com>
2023-02-28 12:37:19 +0100

    suma_maxima (version4)

--- suma_máxima/suma_maxima.cpp
+++ suma_máxima/suma_maxima.cpp
@@ -28,10 +28,14 @@ int main (){
   double sumamaxfilas=0.0, sumamaxcolumnas=0.0, sumamaxdiag1=0.0, sumamaxdiag2=0.0;
 
   std::cout << "The matrix is: \n"  << m << std::endl;
-  std::cout << "La suma maxima de las filas es: " << m.rowwise().sum() << std::endl;
-  std::cout << "La suma maxima de las columnas es: " << m.colwise().sum() << std::endl;
-  std::cout << "La suma maxima de la diagonal_1 es: " << m.diagonal().sum() << std::endl;
-  std::cout << "La suma maxima de la diagonal_2 es: " << m.colwise().reverse().diagonal().sum() << std::endl;
+  std::cout << "La suma maxima de las filas es: " << m.rowwise().sum().maxCoeff() << std::endl;
+  std::cout << "La suma maxima de las columnas es: " << m.colwise().sum().maxCoeff() << std::endl;
   
+  if(m.diagonal().sum() > m.colwise().reverse().diagonal().sum()){
+    std::cout << "La suma maxima de la diagonal es: " << m.diagonal().sum() << std::endl;  
+  } else {
+    std::cout << "La suma maxima de la diagonal es: " << m.colwise().reverse().diagonal().sum() << std::endl;  
+  }
+
   return 0;  
 }
