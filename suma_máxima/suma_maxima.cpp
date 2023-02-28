104ecbcbf9b6f4d38ea4269f6e933949d29e8a37
geerckst <stefano.geercken@gmail.com>
2023-02-28 12:29:02 +0100

    suma_maxima (version3)

--- suma_máxima/suma_maxima.cpp
+++ suma_máxima/suma_maxima.cpp
@@ -2,24 +2,36 @@
 #include <vector>
 #include <Eigen/Dense>
 
-using Eigen::VectorXd;
+//using Eigen::VectorXd;
 using Eigen::MatrixXd;
 
 int main (){
   int rows;
   int sumamax = 0;
   
-  std::cout << "What is the size of the quadratic  matrix? (enter number of rows) " << std::endl;
+  std::cout << "Enter quadratic matrix size: " << std::endl;
   std::cin >> rows;
   
-  MatrixXd m (rows, rows);
+  MatrixXd m(rows, rows);
   
   while(m.rows() != m.cols() && (m.rows()+m.cols()) == m.size()){
       
     std::cout << "That matrix isn't quadratic! " << std::endl;
-    std::cout << "What is the size of the quadratic  matrix? (enter number of rows) " << std::endl;
+    std::cout << "What is the size of the quadratic  matrix? (enter number of rows): " << std::endl;
     std::cin >> rows;    
   }
+
+  m << 1, 2, 3,
+       4, 5, 6,
+       7, 8, 9;
+  
+  double sumamaxfilas=0.0, sumamaxcolumnas=0.0, sumamaxdiag1=0.0, sumamaxdiag2=0.0;
+
+  std::cout << "The matrix is: \n"  << m << std::endl;
+  std::cout << "La suma maxima de las filas es: " << m.rowwise().sum() << std::endl;
+  std::cout << "La suma maxima de las columnas es: " << m.colwise().sum() << std::endl;
+  std::cout << "La suma maxima de la diagonal_1 es: " << m.diagonal().sum() << std::endl;
+  std::cout << "La suma maxima de la diagonal_2 es: " << m.colwise().reverse().diagonal().sum() << std::endl;
   
   return 0;  
 }
